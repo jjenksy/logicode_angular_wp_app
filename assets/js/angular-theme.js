7ee371a684e72e28112ba5d53b7951032635e2f6
jenksy <jjenksy@gmail.com>
2017-03-12 14:49:10 -0500

    added routing and getting of the resource from the server. Moved from usage of cdn to local files

--- assets/js/angular-theme.js
+++ assets/js/angular-theme.js
@@ -1,1 +1,35 @@
-var wpApp = new angular.module('wpAngularTheme',['ui.router']);
+(function () {
+var wpApp = new angular.module( 'wpAngularTheme',
+    ['ui.router',
+        'ngResource'] );
+
+wpApp.factory( 'Posts', function( $resource ) {
+    return $resource( appInfo.api_url + 'posts/:ID', {
+        ID: '@id'
+    })
+});
+
+wpApp.controller( 'ListCtrl', ['$scope', 'Posts', function( $scope, Posts ) {
+    console.log('ListCtrl');
+    $scope.page_title = 'Blog Listing';
+
+    Posts.query().$promise.then(function (resolve) {
+        $scope.posts = resolve;
+    }, function (reject) {
+        console.log(reject);
+
+    })
+
+}]);
+
+    wpApp.config( function( $stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state( 'list', {
+                url: '/',
+                controller: 'ListCtrl',
+                templateUrl: appInfo.template_directory + 'templates/list.html'
+            })
+    })
+
+}());
--- functions.php
+++ functions.php
@@ -3,10 +3,12 @@
 class wp_ng_theme {
 	
 	function enqueue_scripts() {
-		
+
 		wp_enqueue_style( 'bootstrapCSS', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css', array(), '1.0', 'all' );
-		wp_enqueue_script( 'angular-core', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js', array( 'jquery' ), '1.0', false );
-		wp_enqueue_script( 'ui-router', 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min.js', array( 'angular-core' ), '1.0', false );
+		wp_enqueue_script( 'angular-core', get_template_directory_uri() . '/bower_components/angular/angular.min.js', array( 'jquery' ), '1.0', false );
+		wp_enqueue_script( 'angular-resource', get_template_directory_uri() . '/bower_components/angular-resource/angular-resource.min.js', array('angular-core'), '1.0', false );
+		wp_enqueue_script( 'ui-router', get_template_directory_uri() . '/bower_components/angular-ui-router/release/angular-ui-router.min.js', array( 'angular-core' ), '1.0', false );
+		wp_enqueue_script( 'angular-material', get_template_directory_uri() . '/bower_components/angular-material/angular-material.min.js', array( 'angular-core' ), '1.0', false );
 		wp_enqueue_script( 'ngScripts', get_template_directory_uri() . '/assets/js/angular-theme.js', array( 'ui-router' ), '1.0', false );
 		wp_localize_script( 'ngScripts', 'appInfo',
 			array(
