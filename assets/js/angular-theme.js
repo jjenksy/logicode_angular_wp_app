2294fccd39c3b5e45fc294b3f336068f7045fb06
jenksy <jjenksy@gmail.com>
2017-03-18 22:09:06 -0500

    worked on jjScrollTrigger directive. Need to target element that is scrolling not the window itself

--- assets/js/angular-theme.js
+++ assets/js/angular-theme.js
@@ -62,16 +62,27 @@ wpApp.factory( 'Posts', function( $resource ) {
      * The scrolling to directive for angular
      * The function scrollEventCallback() gets called, if it is 100 pixel or less below the currently visible area of the document. Please note,
      * that I used jQuery in this example, so for this to work you will have to include it as well. I hope this is what you asked for.
-     * Todo fix this directive in order to have a scroll to highlight the button 
+     * todo fix the implementation of this
      */
-    wpApp.directive('scrollTrigger', function($window) {
+    wpApp.directive('jjScrollTrigger', function($window) {
         return {
+            /**
+             * @param scope
+             * @param element
+             * @param attrs the attributes on the dom element
+             */
             link : function(scope, element, attrs) {
+                //the offset is the setting for the thresh hold attribute
                 var offset = parseInt(attrs.threshold) || 0;
+                //the target element
                 var e = jQuery(element[0]);
                 var doc = jQuery(document);
                 angular.element(document).bind('scroll', function() {
+                    console.log(doc.scrollTop() + $window.innerHeight + offset);
+                    console.log(e.offset().top);
+
                     if (doc.scrollTop() + $window.innerHeight + offset > e.offset().top) {
+                        //call the scrollTrigger method
                         scope.$apply(attrs.scrollTrigger);
                     }
                 });
--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ class wp_ng_theme {
 	function enqueue_scripts() {
 
 		wp_enqueue_style( 'angular-materialCSS', get_template_directory_uri() . '/bower_components/angular-material/angular-material.min.css', array(), '1.0', 'all' );
+		wp_enqueue_style( 'jquery', get_template_directory_uri() . '/bower_components/jquery/dist/jquery.min.js', array(), '1.0', 'all' );
 		wp_enqueue_script( 'angular-core', get_template_directory_uri() . '/bower_components/angular/angular.min.js', array( 'jquery' ), '1.0', false );
 		wp_enqueue_script( 'angular-resource', get_template_directory_uri() . '/bower_components/angular-resource/angular-resource.min.js', array('angular-core'), '1.0', false );
 		wp_enqueue_script( 'ui-router', get_template_directory_uri() . '/bower_components/angular-ui-router/release/angular-ui-router.min.js', array( 'angular-core' ), '1.0', false );
