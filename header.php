6dc96faec52e421935ee185a865ab566a995d4f1
jenksy <jjenksy@gmail.com>
2017-04-07 13:52:39 -0500

    added playcanvas stuff

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
   	<link rel="apple-touch-icon" href="/favicon.png">
 <!--   wp_head function is used to inject the scripts that were specified in the foote-->
    	<?php wp_head();?>
+    <script src='https://code.playcanvas.com/playcanvas-stable.min.js'></script>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
     <script type="text/javascript">
         //set a global vairable to the templat urls so I can use it later in
@@ -24,7 +25,7 @@
             <md-sidenav
                         class="md-sidenav-left" md-component-id="left"
                         md-whiteframe="4"
-                        md-is-locked-open="$mdMedia('gt-md')">
+                        md-is-locked-open="$mdMedia('gt-sm')">
 
                 <md-toolbar class="md-theme-indigo" layout-align="start center">
                     <div layout-margin layout="column" layout-align="start center">
@@ -36,7 +37,7 @@
                 <md-content layout-margin layout="column">
                     <md-button ng-click="toggleLeft()" ui-sref="About">About Me</md-button>
                     <md-button ng-click="toggleLeft()" ui-sref="skills">Skills</md-button>
-                    <md-button ng-click="toggleLeft()" ui-sref="education">Education</md-button>
+                    <md-button ng-click="toggleLeft()" ui-sref="education">Games</md-button>
                     <md-button ng-click="toggleLeft()" ui-sref="contact">Contact</md-button>
                     <md-button ng-click="toggleLeft()" ui-sref="list">Blog</md-button>
                 </md-content>
@@ -44,17 +45,16 @@
             </md-sidenav>
     </div>
 
-    <md-content flex layout-fill id="jj-scroll-watch">
-        <md-toolbar ng-controller="NavMenuCtrl">
+    <md-content flex layout-fill>
+        <md-toolbar ng-controller="NavMenuCtrl" hide-gt-sm flex>
             <div class="md-toolbar-tools">
 
 
-                <h2 flex md-truncate><?php echo bloginfo('name'); ?></h2>
-
                 <md-button ng-click="toggleLeft()" class="md-icon-button" aria-label="Toggle" hide-gt-md
-                           class="md-accent" >
-                    Toggle left
+                <md-icon  aria-label="menu"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 48 48"><path d="M6 36h36v-4H6v4zm0-10h36v-4H6v4zm0-14v4h36v-4H6z"/></svg></md-icon>
                 </md-button>
+
+
             </div>
         </md-toolbar>
 
